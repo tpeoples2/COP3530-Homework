075e45be4d30e880b671b44edfff408d4470180a
Taylor Peoples <peoples.taylor@gmail.com>
2012-02-19 23:58:41 -0500

    Changed file input to read in multi-word entries using getline().

--- HW2/hw2prob4.cpp
+++ HW2/hw2prob4.cpp
@@ -1,3 +1,8 @@
+/*
+ * @name    Taylor Peoples
+ * @UFID    1136-2951
+ */
+
 #include <iostream>
 #include <fstream>
 #include <algorithm>
@@ -13,34 +18,43 @@ int main() {
     
     arrayList<string> dictionaryArrayList; 
     
+
+
     int i = 0;
     while (inFile) {
         string temp;
         inFile >> temp;
+        // check if the entry has multiple words
+        if (char(inFile.peek()) == ' ') {
+            string temp2;
+            getline(inFile, temp2);
+            temp += temp2;
+        }
+        // check if the file has reached the end
         if (temp.compare("") == 0) {
             continue;
         }
         dictionaryArrayList.insert(i, temp);
-        i++;
+        i++;   
     }
     
     inFile.close();
 
     sort(dictionaryArrayList.beginRA(), dictionaryArrayList.endRA());
 
-    ofstream outFile("SortedDictionary");
+    ofstream outFile("sortedDictionary");
     
     if (!outFile) {
-        cerr << "Error creating file 'SortedDictionary'. Exiting" << endl;
+        cerr << "Error creating file 'sortedDictionary'. Exiting" << endl;
         return 1;
     }
     dictionaryArrayList.output(outFile);
     outFile.close();
     
     // testing [] operator
-    cout << dictionaryArrayList[0] << endl; // should print 'a'
-    dictionaryArrayList[0] = "TEST";
-    cout << dictionaryArrayList[0] << endl; // should print 'TEST'
+    //cout << dictionaryArrayList[0] << endl; // should print 'a'
+    //dictionaryArrayList[0] = "TEST";
+    //cout << dictionaryArrayList[0] << endl; // should print 'TEST'
 
     return 0;
 }
